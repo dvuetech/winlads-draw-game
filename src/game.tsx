1aeb7f7a18190e994bab02e2afb7cb834409d71f
PandukaNandara <pandukanandara@outlook.com>
2025-01-10 01:32:16 +0530

    WINL-723 Refactor game component to utilize Next.js search parameters for data fetching. Added loading states for giveaway and entries, and implemented missing values check for query parameters. Improved overall data management and error handling during API calls.

--- src/game.tsx
+++ src/game.tsx
@@ -3,7 +3,11 @@ import Phaser, { Game as GameType } from "phaser";
 import { useEffect, useState } from "react";
 import PreloadScene from "./scenes/preloadScene";
 import MainScene from "./scenes/MainScene";
-import { GameDataAdmin, GetEntryBalanceCombinedResponse } from "./models/game-data.model";
+import { useSearchParams } from "next/navigation";
+import {
+  GameDataAdmin,
+  GetEntryBalanceCombinedResponse,
+} from "./models/game-data.model";
 import GiveawayService from "./services/GiveawayService"; // Import the GiveawayService
 import EntryService from "./services/EntryService";
 
@@ -15,44 +19,63 @@ const Game = () => {
   const [data, setData] = useState<GameDataAdmin>();
   const [giveaway, setGiveaway] = useState<any>(null); // State for storing giveaway data
   const [urlParams, setUrlParams] = useState<any>(null);
-  const [entries, setEntries] = useState<GetEntryBalanceCombinedResponse | null>(null);
-  const [loadingEntries, setLoadingEntries] = useState(false);
+  const [entries, setEntries] =
+    useState<GetEntryBalanceCombinedResponse | null>(null);
+  const [loadingEntries, setLoadingEntries] = useState(true);
+  const [giveawayLoading, setGiveawayLoading] = useState(true);
 
+  const searchParams = useSearchParams();
   // Extract query parameters
+  const [missingValues, setMissingValues] = useState<string[]>([]);
   useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
+    const accessToken = searchParams.get("accessToken");
+    const winUrl = searchParams.get("winUrl");
+    const adminUrl = searchParams.get("adminUrl");
+    const giveawayId = searchParams.get("giveawayId");
     const extractedParams = {
-      accessToken: queryParams.get("accessToken"),
-      winUrl: queryParams.get("winUrl"),
-      adminUrl: queryParams.get("adminUrl"),
-      giveawayId: queryParams.get("giveawayId"),
+      accessToken,
+      winUrl,
+      adminUrl,
+      giveawayId,
     };
+    const missingValues: string[] = [];
+    Object.entries(extractedParams).forEach(([key, value]) => {
+      if (!value) {
+        missingValues.push(key);
+      }
+    });
+    setMissingValues(missingValues);
     setUrlParams(extractedParams);
-  }, []);
+  }, [searchParams]);
 
-  // Fetch giveaway details using GiveawayService
+  // Fetch entry details and giveaway details
   useEffect(() => {
     const fetchGiveaway = async () => {
       if (urlParams?.giveawayId) {
+        setGiveawayLoading(true);
         try {
-          const response = await GiveawayService.getGiveaway(urlParams.accessToken, urlParams.winUrl, urlParams.giveawayId);
+          const response = await GiveawayService.getGiveaway(
+            urlParams.accessToken,
+            urlParams.winUrl,
+            urlParams.giveawayId
+          );
           setGiveaway(response); // Store the fetched giveaway data
         } catch (error) {
           console.error("Error fetching giveaway:", error);
+        } finally {
+          setGiveawayLoading(false);
         }
       }
     };
-
-    fetchGiveaway();
-  }, [urlParams?.giveawayId, urlParams?.winUrl]);
-
-  // Fetch entry details
-  useEffect(() => {
     const fetchEntries = async () => {
       if (urlParams?.giveawayId) {
         setLoadingEntries(true);
         try {
-          const response = await EntryService.getEntries(urlParams.accessToken, urlParams.adminUrl, urlParams.giveawayId);
+          const response = await EntryService.getEntries(
+            urlParams.accessToken,
+            urlParams.adminUrl,
+            urlParams.giveawayId
+          );
           setEntries(response);
         } catch (error) {
           console.error("Error fetching entries:", error);
@@ -61,9 +84,9 @@ const Game = () => {
         }
       }
     };
-
+    fetchGiveaway();
     fetchEntries();
-  }, [urlParams?.giveawayId, urlParams?.adminUrl]);
+  }, [urlParams]);
 
   useEffect(() => {
     if (giveaway && entries && urlParams?.accessToken) {
@@ -72,13 +95,13 @@ const Game = () => {
         accessToken: urlParams.accessToken!,
         winnerSubmitUrl: urlParams.winUrl!,
         giveawayId: urlParams.giveawayId!,
-        entries: entries?.data, 
-        giveaway: giveaway, 
+        entries: entries?.data,
+        giveaway: giveaway,
       };
-      setData(gameDataAdmin); 
+      setData(gameDataAdmin);
     }
   }, [giveaway, entries, urlParams]);
-  
+
   // Initialize Phaser game
   useEffect(() => {
     if (!game && data) {
@@ -120,28 +143,11 @@ const Game = () => {
     }
   }, [game, data]);
 
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      const data = event.data;
-      if (data.type === "DRAW_GIVEAWAY") {
-        setData(data);
-        setTimeout(() => {
-          setLoading(false);
-        }, 100);
-      }
-    };
-    window.addEventListener("message", handleMessage);
-  }, []);
+  if (missingValues.length) {
+    return <div>Missing params: {missingValues.join(", ")}</div>;
+  }
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  if (loading) {
+  if (loadingEntries && giveawayLoading) {
     return <div>Loading...</div>;
   }
 
